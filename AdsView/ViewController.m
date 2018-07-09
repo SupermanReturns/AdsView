6c15b78b68d0ff0ba36064482de9745055456a33
Superman <703353105@qq.com>
2018-07-09 14:56:37 +0800

    修改代码

--- AdsView/ViewController.m
+++ AdsView/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
+#import "AdsView.h"
 
-@interface ViewController ()
+@interface ViewController ()<AdsViewDelegate>
 
 @end
 
@@ -21,7 +23,47 @@
     [self performSelector:@selector(showAdsView)
                withObject:nil
                afterDelay:2];
+}
+- (void)showAdsView{
+    //    [_infoCard show];
+    
+    AppDelegate *app = [UIApplication sharedApplication].delegate;
+    app.window.backgroundColor = [UIColor colorWithWhite:20
+                                                   alpha:0.3];
+    AdsView *adsView = [[AdsView alloc] initWithWindow:app.window];
+    adsView.tag = 10;
+    adsView.delegate = self;
+    
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    for (int i = 0; i < 2; i ++) {
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0,adsView.mainContainView.frame.size.width, adsView.mainContainView.frame.size.width)];
+        label.font = [UIFont boldSystemFontOfSize:20.0];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.text = [NSString stringWithFormat:@"视图 %d", i+1];
+        label.textColor = [UIColor redColor];
+        label.backgroundColor = [UIColor colorWithRed:0 green:183.0/255.0 blue:238.0/255.0 alpha:1.000];
+        label.layer.cornerRadius = adsView.mainContainView.frame.size.width/2;
+        label.layer.masksToBounds = YES;
+        [array addObject:label];
+    }
+    [self.view addSubview:adsView];
+    adsView.containerSubviews = array;
+    [adsView showAnimated:YES];
+}
 
+- (void)hide{
+    AdsView *adsView = (AdsView *)[self.view viewWithTag:10];
+    [adsView hideAnimated:YES];
+}
+- (void)AdsViewDidAppear:(AdsView *)view{
+    NSLog(@"视图出现");
+}
+- (void)AdsViewDidDisAppear:(AdsView *)view{
+    NSLog(@"视图消失");
+}
+
+- (void)AdsViewTapMainContainView:(AdsView *)view currentSelectIndex:(long)selectIndex{
+    NSLog(@"点击主内容视图:--%ld",selectIndex);
 }
 
 
